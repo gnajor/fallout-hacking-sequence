4284f11415dbfb2595a09c4f49139073a0d2cff7
Leo Mühl <143512644+gnajor@users.noreply.github.com>
2026-07-10 16:16:38 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
+
+    $effect(() => {
+        const onKeydown = () => {
+            goto("/hacking");
+        };
+        window.addEventListener("keydown", onKeydown, { once: true });
+        window.addEventListener("click", onKeydown, {once: true});
+    });
 </script>
 
-
-
 <div id="terminal">
-    <video autoplay loop>
+    <video autoplay loop muted>
         <source
             src="/videos/fallout-video-smaller.mp4"
             type="video/mp4"
-
         >
     </video>
     <p>Press any button to start</p>
+</div>
+<div id="second-screen">
+    <img src="" alt="">
+    <img src="" alt="">    
 </div>
 
 <style>
@@ -33,9 +42,11 @@
         color: var(--main-color);
         position: absolute;
         bottom: 0;
-        font-size: 1.25rem;
+        font-size: 1.5vw;
         right: 30%;
         bottom:10%;
+        filter: blur(1px);
+        text-shadow: 2px 2px black;
     }
 </style>
 
