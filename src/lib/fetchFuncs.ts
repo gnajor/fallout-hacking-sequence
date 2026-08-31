6070973280ee3049080d4bb9257a393f85cdc441
gnajor <lejonet04@gmail.com>
2026-08-31 13:54:12 +0200

    updated readme

--- src/lib/fetchFuncs.ts
+++
@@ -1,9 +0,0 @@
-export async function submitScore(name: string, bestRun: number): Promise<void>{
-    await fetch("api/score", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({name, bestRun}),
-        credentials: "same-origin"
-    });
-}
-
